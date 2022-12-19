20aff7a1b07b1e33833bcf4fd6cf1fd215445971
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2022-12-19 19:15:25 +0700

    213

--- Week1/unittest_test/Ex5Unittest.py
+++ Week1/unittest_test/Ex5Unittest.py
@@ -11,12 +11,20 @@ class Sum0TestCase(unittest.TestCase):
         assert result == [[-3, 1, 2]]
 
     def test_main2(self):
-        result = Sum0([-5, 2, 9, 7, 1, -2, 0])
-        assert result == [[-5, -2, 7], [-2, 0, 2]]
+        result = Sum0([-5, 2, 3, 9, 7, 1, -2, 0])
+        assert result == [[-5, -2, 7], [-5, 2, 3], [-2, 0, 2]]
+
+    def test_main3(self):
+        result = Sum0([-1, 0, 1, -1, 2])
+        assert result == [[-1, -1, 2], [-1, 0, 1], [-1, 0, 1]]
+
+    def test_false(self):
+        result = Sum0([998, 231, 355, 4321, 4314123, 3213123, 45320, 30219, 3210, 09321, 30219])
+        assert result == "No Answer"
 
     def test_main0(self):
         result = Sum0([])
-        assert result == []
+        assert result == "No Answer"
 
 
 def Sum0(Array):
@@ -27,12 +35,15 @@ def Sum0(Array):
         while (PF < PL):
             if (Array[i] + Array[PF] + Array[PL] == 0):
                 result.append([Array[i], Array[PF], Array[PL]])
-                break
+                PF += 1
             elif (Array[i] + Array[PF] + Array[PL] > 0):
                 PL -= 1
             elif (Array[i] + Array[PF] + Array[PL] < 0):
                 PF += 1
-    return result
+    if result == []:
+        return "No Answer"
+    else:
+        return result
 
 
 if __name__ == '__main__':
