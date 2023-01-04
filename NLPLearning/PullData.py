8f5b2cdf8bf686de351b2ba39960134479e61e88
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2023-01-04 10:19:02 +0700

    21

--- NLPLearning/PullData.py
+++ NLPLearning/PullData.py
@@ -5,6 +5,7 @@ from pythainlp.corpus import thai_stopwords
 import emoji
 import re
 
+
 class PullTweetsData():
     def __init__(self):
         self.__count = 0
@@ -48,30 +49,31 @@ class PullTweetsData():
                 self.__count = 0
                 break
 
-    def removeSpecialChar(self,text):
-        return re.sub(r"[!@#$%+:\"]","",text)
+    def removeSpecialChar(self, text):
+        return re.sub(r"[!@#$%+:\"]", "", text)
 
-    def removeEmoji(self,text):
+    def removeEmoji(self, text):
         allchars = [str for str in text]
         emoji_list = [c for c in allchars if c in emoji.EMOJI_DATA]
         return ''.join([str for str in allchars if not any(i in str for i in emoji_list)])
 
-    def removeLink(self,text):
+    def removeLink(self, text):
         return re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
 
-    def removeEnglish(self,text):
-        return re.sub('[^ก-๙]',"",text)
+    def removeEnglish(self, text):
+        return re.sub('[^ก-๙]', "", text)
 
     def saveTweets(self, excelFileName):
         self.__df.to_excel(f"{excelFileName}.xlsx")
 
-    def preprocessText(self,text):
+    def preprocessText(self, text):
         text = self.removeEmoji(text)
         text = self.removeSpecialChar(text)
         text = self.removeLink(text)
         text = self.removeEnglish(text)
         SplitedSentence = word_tokenize(text, engine="newmm")
-        result = [word for word in SplitedSentence if word not in list(thai_stopwords()) and " " not in word]
+        result = [word for word in SplitedSentence if word not in list(
+            thai_stopwords()) and " " not in word]
         return " /".join(result)
 
     def textSplit(self):
@@ -79,19 +81,20 @@ class PullTweetsData():
             print(self.preprocessText(i))
             print("=============================")
 
+
 api_key = "SnxucIPt1fg7UUyVOT0T5j0pR"
 api_key_secret = "yaToQPv95OA1fiNTHD8drKM8g8rZGM7jSQnPOLoxU3QA9UpaLm"
 access_token = "1722424471-Xb0DjPVOqXsLj2sXEYXmU2sqxaDC4B793erGO6J"
 access_token_secret = "D2LyXN11zAoZB0M476eb1ZGDM55oRvy4tBWNb8pR4CO0h"
 
-puller = PullTweetsData()
+# puller = PullTweetsData()
 
-puller.getAccessToAPI(api_key, api_key_secret)
-puller.setUserAuthentication(access_token, access_token_secret)
+# puller.getAccessToAPI(api_key, api_key_secret)
+# puller.setUserAuthentication(access_token, access_token_secret)
 
-puller.getTwitterAPI()
-puller.createDataFrame()
+# puller.getTwitterAPI()
+# puller.createDataFrame()
 
-puller.pullTweets("ปีเก่า", 10)
+# puller.pullTweets("ปีเก่า", 10)
 
-print(puller.textSplit())
+# print(puller.textSplit())
