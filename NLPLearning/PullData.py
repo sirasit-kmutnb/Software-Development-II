4ab8a1838e250c42b8a1938586dbd084faf5d4a5
nonthawat-kmutnb <s6401012610018@email.kmutnb.ac.th>
2023-01-07 19:30:10 +0700

    Update PullData.py

--- NLPLearning/PullData.py
+++ NLPLearning/PullData.py
@@ -129,7 +129,7 @@ puller.setUserAuthentication(access_token, access_token_secret)
 puller.getTwitterAPI()
 puller.createDataFrame()
 
-puller.pullTweets("ปีเก่า", 50)
+puller.pullTweets("หุ้น", 500)
 
 puller.connectToDB("twitter", "tweets")
 puller.saveTweets()
