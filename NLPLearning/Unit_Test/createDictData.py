1710118f27ba1215ca062487f12058ef2095acce
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2023-01-10 20:24:53 +0700

    1

---
+++ NLPLearning/Unit_Test/createDictData.py
@@ -0,0 +1,14 @@
+import PullDataV1
+import unittest
+
+Puller = PullTweetsData()
+
+
+class TestcreateDictData(unittest.TestCase):
+    def test_main(self):
+        result =
+        assert result == [[-3, 1, 2], [-2, 0, 2], [-1, 0, 1]]
+
+
+if __name__ == '__main__':
+    unittest.main()
