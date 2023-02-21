21cb3aea3c12a1a25c8fa5dbd1dfa889e0452e44
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2023-02-21 22:49:22 +0700

    .

--- TwitterKeeper/Twitter_keeper.py
+++ TwitterKeeper/Twitter_keeper.py
@@ -78,6 +78,9 @@ class PullTweetsData():
 
     def pullTweetsThread(self, query, amount):
         count = 0
+        start_date = "2023-01-01"
+        end_date = "2023-01-15"
+        # , since_id=start_date, max_id=end_date
         for tweet in tweepy.Cursor(self.__api.search_tweets, q=query, count=100,
                                    result_type="recent", tweet_mode='extended').items():
             entity_hashtag = tweet.entities.get('hashtags')
@@ -205,8 +208,8 @@ def pullTweetsTask():
     pullerT1.getAccessToAPI(api_key, api_key_secret)
     pullerT1.setUserAuthentication(access_token, access_token_secret)
     pullerT1.getTwitterAPI()
-    pullerT1.connectToDB("twitter_keeper", "tweets")
-    t1 = Thread(target=pullerT1.pullTweets, args=("#breakingnews", 1000))
+    pullerT1.connectToDB("twitter_keeper", "tweets_test")
+    t1 = Thread(target=pullerT1.pullTweets, args=("#happynewyear", 1000))
     t1.start()
 
     # A = pullerT1.find_multi("", "", "", "Bangkok", "",
@@ -219,4 +222,4 @@ def pullTweetsTask():
     # pullerT1.find_tweets("text","ยู")
 
 
-# pullTweetsTask()
+pullTweetsTask()
