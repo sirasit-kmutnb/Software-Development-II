75cbb1654115ccf263e95fe551adc65e2a0c0d67
nonthawat-kmutnb <s6401012610018@email.kmutnb.ac.th>
2023-03-20 23:52:23 +0700

    .

--- TwitterKeeper/test_top10word.py
+++ TwitterKeeper/test_top10word.py
@@ -1,13 +1,20 @@
 from Twitter_Analyzer import FindTopWord
 import unittest
 from unittest.mock import MagicMock
+from sklearn.feature_extraction.text import CountVectorizer
 
 class TestMostTop10Word(unittest.TestCase):
 
     def setUp(self) -> None:
+        # self.c = CountVectorizer()
+        # CountVectorizer = MagicMock()
         self.anlz = FindTopWord()
 
+    # def tearDown(self) -> None:
+    #     CountVectorizer = self.c
+
     def test_tokenize(self):
+        
         mock_d = "สอบ/ข้อสอบ/A-Level/Eng"
         expected_result = ['สอบ', 'ข้อสอบ', 'A-Level','Eng']
         result = self.anlz.tokenize(mock_d)
@@ -19,10 +26,43 @@ class TestMostTop10Word(unittest.TestCase):
             {'text': 'ในที่สุดก็จะได้เล่นเกมเเล้ว😭😭'},
             {'text': 'ไม่ต้องรอ🔥'}
         ]
-        mock_preprocessed_text = ['เจอโจทย์แล้วเหม่อเลย', 'ในที่สุดก็จะได้เล่นเกมเเล้ว', 'ไม่ต้องรอ']
-        self.anlz.preprocessText = MagicMock(return_value=mock_preprocessed_text)
+        self.anlz.preprocessText = MagicMock(return_value="preprocessed_text")
         result = self.anlz.prepared_Text(mock_text_list)
-        self.assertEqual(result, mock_preprocessed_text)
+        self.assertEqual(result, ["preprocessed_text","preprocessed_text","preprocessed_text"])
+
+    # def test_Most10WordFinder(self):
+        # # Mock the necessary objects
+        # CountVectorizer = MagicMock()
+        # CountVectorizer.fit_transform.return_value = "transformed_data"
+        # CountVectorizer.get_feature_names_out.return_value = ["word1", "word2", "word3"]
+        
+        # # Call the method with the mocked objects
+        # result = self.anlz.Most10WordFinder([{'text':"tweet1"},{'text':"tweet2"}])
+        
+        # # Check the result
+        # self.assertEqual(result.to_dict(), {
+        #     'word': ['word1', 'word2', 'word3'],
+        #     'count': [1, 1, 1],
+        # })
+    # def test_most_10_word_finder(self):
+    #     # create a mock object for the CountVectorizer
+    #     mock_vectorizer = MagicMock()
+    #     mock_vectorizer.get_feature_names_out.return_value = ['apple', 'banana','juice', 'fruit']
+    #     mock_transformed_data = MagicMock()
+    #     mock_transformed_data.sum.return_value = [10, 20, 30, 40]
+    #     mock_vectorizer.fit_transform.return_value = mock_transformed_data
+
+    #     # call the method with the mock object
+    #     result = self.anlz.Most10WordFinder([{'text':'apple juice'}, {'text':'banana fruit'}])
+
+    #     # assert the result
+    #     self.assertEqual(result['word'].tolist(), ['apple','juice', 'banana','fruit'])
+    #     self.assertEqual(result['count'].tolist(), [40,30,20,10])
+
+    #     # assert that the mock object was called correctly
+    #     mock_vectorizer.fit_transform.assert_called_once_with(self.anlz.prepared_Text(['I love apple juice', 'banana is my favorite fruit']))
+    #     mock_vectorizer.get_feature_names_out.assert_called_once()
+
 
 
 if __name__ == "__main__":
